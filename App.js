1f0bd3f79bccfed2df3772f86c4f80be406d4d9d
iruka16 <62045134+iruka16@users.noreply.github.com>
2023-09-02 22:29:39 -0400

    little chg

--- App.js
+++ App.js
@@ -1,13 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
-
+import { LinearGradient } from 'expo-linear-gradient';
 import StartGameScreen from './screens/StartGameScreen';
 
 export default function App() {
   return (
-   <View style = {styles.rootScreen}>
+   <LinearGradient colors={['#ddb52f', '#4e0329']} style = {styles.rootScreen}>
   <StartGameScreen/>
-   </View>
+   </LinearGradient>
     
   );
 }
